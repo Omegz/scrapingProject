d3d8a059fceb8f3dcd5fc124400ab5ebe32e2699
MahmoodKhalil57 <mahmoodihabk@gmail.com>
2023-09-15 00:42:23 +0100

    push table

--- src/routes/testCrypto/+page.svelte
+++ src/routes/testCrypto/+page.svelte
@@ -1,37 +1,28 @@
 <script lang="ts">
+	import Table from '$lib/table.svelte';
 	import { onMount } from 'svelte';
 
-	type ResponseType = {
-		binanceData: {
-			symbol: string;
-			markPrice: number;
-			indexPrice: number;
-			estimatedSettlePrice: number;
-			lastFundingRate: number;
-			nextFundingTime: string;
-			interestRate: number;
-			time: string;
-		}[];
-		binanceFundingRateData: {
-			symbol: string;
-			fundingRate: number;
-			fundingTime: string;
-		}[];
-		okexFundingRateData: {
-			instId: string;
-			instType: string;
-			fundingRate: number;
-			nextFundingRate: number;
-			fundingTime: string;
-			nextFundingTime: string;
-		}[];
-	};
+	let data: any | undefined;
 
-	let data: ResponseType | undefined;
+	let dataMeta = [
+		{
+			title: 'Binance Data',
+			dataKey: 'binanceData'
+		},
+		{
+			title: 'Binance Funding Rate Data',
+			dataKey: 'binanceFundingRateData'
+		},
+		{
+			title: 'Okex Funding Rate Data',
+			dataKey: 'okexFundingRateData'
+		}
+	];
 
 	const fetchData = async () => {
 		try {
 			const response = await fetch('http://127.0.0.1:5000/api/crypto-data'); // Update the URL accordingly
+			console.log('fetched');
 			if (response.ok) {
 				data = await response.json();
 			} else {
@@ -62,78 +53,14 @@
 			favoriteColor: 'Red'
 		}
 	];
-
-  let getBinanceHeaders = () => {
-    
-  }
 </script>
 
 <main>
 	{#if data && Object.keys(data).length > 0}
-		<h1>Binance Data:</h1>
-		<div class="overflow-x-auto">
-			<table class="table">
-				<!-- head -->
-				<thead>
-					<tr>
-						<th />
-						{#each titles as title}
-							<th>{title}</th>
-						{/each}
-					</tr>
-				</thead>
-				<tbody>
-					<!-- row 1 -->
-					{#each dataB as row, i}
-						<tr>
-							<th>1</th>
-							<td>{row.name}</td>
-							<td>{row.job}</td>
-							<td>{row.favoriteColor}</td>
-						</tr>
-					{/each}
-				</tbody>
-			</table>
-		</div>
-		<ul>
-			{#each data.binanceData as binanceItem}
-				<li class="p-6 bg-green-200">
-					<p>Symbol: {binanceItem.symbol}</p>
-					<p>Mark Price: {binanceItem.markPrice}</p>
-					<p>Index Price: {binanceItem.indexPrice}</p>
-					<p>Estimated Settle Price: {binanceItem.estimatedSettlePrice}</p>
-					<p>Last Funding Rate: {binanceItem.lastFundingRate}</p>
-					<p>Next Funding Time: {binanceItem.nextFundingTime}</p>
-					<p>Interest Rate: {binanceItem.interestRate}</p>
-					<p>Time: {binanceItem.time}</p>
-				</li>
-			{/each}
-		</ul>
-
-		<h1>Binance Funding Rate Data:</h1>
-		<ul>
-			{#each data.binanceFundingRateData as fundingRateItem}
-				<li class="p-6 bg-yellow-200">
-					<p>Symbol: {fundingRateItem.symbol}</p>
-					<p>Funding Rate: {fundingRateItem.fundingRate}</p>
-					<p>Funding Time: {fundingRateItem.fundingTime}</p>
-				</li>
-			{/each}
-		</ul>
-
-		<h1>Okex Funding Rate Data:</h1>
-		<ul>
-			{#each data.okexFundingRateData as okexRate}
-				<li class="p-6 bg-blue-200">
-					<p>Instrument ID: {okexRate.instId}</p>
-					<p>Instrument Type: {okexRate.instType}</p>
-					<p>Funding Rate: {okexRate.fundingRate}</p>
-					<p>Next Funding Rate: {okexRate.nextFundingRate}</p>
-					<p>Funding Time: {okexRate.fundingTime}</p>
-					<p>Next Funding Time: {okexRate.nextFundingTime}</p>
-				</li>
-			{/each}
-		</ul>
+		{#each dataMeta as meta}
+			<h1>{meta.title}:</h1>
+			<Table data={data[meta.dataKey]} />
+		{/each}
 	{:else}
 		<p>No data available.</p>
 	{/if}
